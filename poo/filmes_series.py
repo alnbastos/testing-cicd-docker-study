9e9e3154fc7c50d00c3b9c623603900602f04cf3
aline bastos <alinebastos23@outlook.com>
2023-02-27 23:31:51 -0300

    uncomplicating Playlist class + Duck Typing

--- poo/filmes_series.py
+++ poo/filmes_series.py
@@ -41,10 +41,22 @@ class Serie(Programa):
         return f'{self._nome} - {self.ano} - {self.temporadas} temp: {self._likes} Likes'
 
 
-class Playlist(list):
+class Playlist:
     def __init__(self, nome, programas):
         self.nome = nome
-        super().__init__(programas)
+        self._programas = programas
+
+    def __getitem__(self, item):
+        return self._programas[item]
+        
+    @property
+    def listagem(self):
+        return self._programas
+    
+    @property
+    def tamanho(self):
+        return len(self._programas)
+    
 
 
 vingadores = Filme('vingadores - guerra infinita', 2018, 160)
@@ -62,7 +74,9 @@ demolidor.dar_likes(), demolidor.dar_likes()
 filmes_e_series = [vingadores, atlanta, demolidor, tmep]
 playlist_fim_de_semana = Playlist('Fim de semana', filmes_e_series)
 
-print(f'Tamanho da playlist: {len(playlist_fim_de_semana)}')
+print(f'Tamanho da playlist: {len(playlist_fim_de_semana.listagem)}')
+
+print(f'{vingadores.nome} está na Playlist? {"Sim" if vingadores in playlist_fim_de_semana else "Não"}')
 
 for programa in playlist_fim_de_semana:
     print(programa)
