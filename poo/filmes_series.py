a3dcc0fb4e7dacc9326cab9a3817842c4d86d544
aline bastos <alinebastos23@outlook.com>
2023-02-27 22:19:51 -0300

    update Playlist class with list inheritance

--- poo/filmes_series.py
+++ poo/filmes_series.py
@@ -41,13 +41,10 @@ class Serie(Programa):
         return f'{self._nome} - {self.ano} - {self.temporadas} temp: {self._likes} Likes'
 
 
-class Playlist:
+class Playlist(list):
     def __init__(self, nome, programas):
         self.nome = nome
-        self.programas = programas
-
-    def tamanho(self):
-        return len(self.programas)
+        super().__init__(programas)
 
 
 vingadores = Filme('vingadores - guerra infinita', 2018, 160)
@@ -65,5 +62,7 @@ demolidor.dar_likes(), demolidor.dar_likes()
 filmes_e_series = [vingadores, atlanta, demolidor, tmep]
 playlist_fim_de_semana = Playlist('Fim de semana', filmes_e_series)
 
-for programa in playlist_fim_de_semana.programas:
+print(f'Tamanho da playlist: {len(playlist_fim_de_semana)}')
+
+for programa in playlist_fim_de_semana:
     print(programa)
