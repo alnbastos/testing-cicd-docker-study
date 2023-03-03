5a48284ba19b23607a943cb0fee8247db22966c7
aline bastos <alinebastos23@outlook.com>
2023-03-02 22:14:20 -0300

    mixins

--- poo/funcionarios.py
+++ poo/funcionarios.py
@@ -1,4 +1,7 @@
 class Funcionario:
+    def __init__(self, nome) -> None:
+        self.nome = nome
+
     def registra_horas(self, horas):
         print('Horas registradas...')
 
@@ -22,6 +25,11 @@ class Alura(Funcionario):
         print('Mostrando perguntas não respondidas do fórum.')
 
 
+class Hipster:
+    def __str__(self) -> str:
+        return f'Hipster - {self.nome}'
+
+
 class Junior(Alura):
     pass
 
@@ -30,11 +38,17 @@ class Pleno(Alura, Caelum):
     pass
 
 
-jose = Junior()
+class Senior(Alura, Caelum, Hipster):
+    pass
+
+jose = Junior('jose')
 jose.busca_perguntas_sem_resposta()
 # jose.busca_cursos_do_mes() # Não funciona pois não está sendo herdado
 
-luan = Pleno()
+luan = Pleno('luan')
 luan.busca_perguntas_sem_resposta()
 luan.busca_cursos_do_mes()
 luan.mostra_tarefas()
+
+gui = Senior('Gui')
+print(gui)
