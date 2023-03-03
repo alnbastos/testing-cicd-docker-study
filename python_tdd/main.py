6f1d99390cb70a862676780f4df99e886fb2cb3d
aline bastos <alinebastos23@outlook.com>
2023-03-03 00:34:45 -0300

    starting automated tests

--- python_tdd/main.py
+++ python_tdd/main.py
@@ -1,4 +1,10 @@
 from funcionario import Funcionario
 
-aline = Funcionario('Aline', '03/12/1998', 1_000)
-print(aline.idade())
+# aline = Funcionario('Aline', '03/12/1998', 1_000)
+# print(aline.idade())
+
+def teste_idade():
+    funcionario_teste = Funcionario('Teste', '03/12/1998', 1_111)
+    print(f'Teste = {funcionario_teste.idade()}')
+
+teste_idade()
