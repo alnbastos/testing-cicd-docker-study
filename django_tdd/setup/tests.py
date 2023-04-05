791caf11e59166296d2e6b4c2520afd57782e750
Aline Bastos <alinebastos23@outlook.com>
2023-04-04 22:26:01 -0300

    adding firefox driver and creating tests

--- django_tdd/setup/tests.py
+++ django_tdd/setup/tests.py
@@ -4,7 +4,15 @@ from selenium import webdriver
 
 class AnimaisTestCase(LiveServerTestCase):
     def setUp(self):
-        self.driver = webdriver.Firefox(executable_path=GeckoDriverManager().install())
+        # self.driver = webdriver.Firefox(executable_path=GeckoDriverManager(cache_valid_range=10).install())
+        self.driver = webdriver.Firefox(executable_path='driver/geckodriver.exe')
 
     def tearDown(self):
         self.driver.quit()
+
+    def test_abre_janela_do_browser(self):
+        self.driver.get(self.live_server_url)
+
+    def test_falho_de_proposito(self):
+        """teste de exemplo de erro"""
+        self.fail('Teste falhou')
