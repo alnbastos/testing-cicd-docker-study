7144dda9d31f02f6cd54b48a8311735dc0134ab8
Aline Bastos <alinebastos23@outlook.com>
2023-04-04 21:50:05 -0300

    adding selenium in the project

--- django_tdd/setup/tests.py
+++ django_tdd/setup/tests.py
@@ -1,6 +1,10 @@
 from django.test import LiveServerTestCase
+from webdriver_manager.firefox import GeckoDriverManager 
 from selenium import webdriver
 
 class AnimaisTestCase(LiveServerTestCase):
     def setUp(self):
-        pass
+        self.driver = webdriver.Firefox(executable_path=GeckoDriverManager().install())
+
+    def tearDown(self):
+        self.driver.quit()
