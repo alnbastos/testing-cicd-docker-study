d6c02926586a307b8db674d2bb837ca1eb0e88af
Aline Bastos <alinebastos23@outlook.com>
2023-07-06 23:35:45 -0300

    feat: create animals

--- docker/app/main.py
+++ docker/app/main.py
@@ -1,7 +1,14 @@
 from fastapi import FastAPI
+from app.schemas import Animals
 
 app = FastAPI()
 
 @app.get("/")
 def read_root():
     return {"message": "Hello World"}
+
+
+@app.post("/create/animal/")
+def create_animal(animal: Animals):
+    return dict(animal)
+
